720418145d094f0397c99aaa15bc7adfa2a78461
vartikadhochak <dhochakvartika@gmail.com>
2026-02-17 18:35:19 +0530

    Create TwoSum.java
    Added Two Sum problem using HashMap

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class TwoSum {
+
+    // Returns indices of the two numbers such that they add up to target
+    public static int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
